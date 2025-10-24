f19e4b7760ebc8349f78556d8053ce30210a8928
Love Kharod <kharodl@gmail.com>
2025-10-23 17:52:56 -0700

    go back to right hand upper arrow keys

--- keyboards/planck/keymaps/default/keymap.c
+++ keyboards/planck/keymaps/default/keymap.c
@@ -79,17 +79,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Enter |MsnCtl| Home |  Up  | End  | MS_1 |   *  |   4  |   5  |   6  |   +  |  =   |
+ * |Enter |MsnCtl| Home |  Up  | End  |PageDn|   *  |   4  |   5  |   6  |   +  |  =   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |PrtScn| Left | Down |Right | MS_2 |   /  |   1  |   2  |   3  |   -  |      |
+ * |      |PrtScn| Left | Down |Right |PageUp|   /  |   1  |   2  |   3  |   -  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |------|             |      |   0  |   .  |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    _______,
-    KC_ENT,  KC_MCTL, KC_HOME, KC_UP,   KC_END,  MS_BTN1, KC_ASTR, KC_4,    KC_5,    KC_6,   KC_PLUS, KC_EQL,
-    _______, KC_PSCR, KC_LEFT, KC_DOWN, KC_RGHT, MS_BTN2, KC_SLSH, KC_1,    KC_2,    KC_3,   KC_MINS, _______,
+    KC_ENT,  KC_MCTL, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_ASTR, KC_4,    KC_5,    KC_6,   KC_PLUS, KC_EQL,
+    _______, KC_PSCR, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_SLSH, KC_1,    KC_2,    KC_3,   KC_MINS, _______,
     _______, _______, _______, KC_MCTL, _______, _______, _______, _______, KC_0,    KC_DOT, _______, _______
 ),
 
@@ -97,17 +97,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | PgUp |   _  |   +  |   [  |   ]  |  \   |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  Up  |   _  |   +  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |PgDown|   -  |   =  |   {  |   }  |  |   |
+ * |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Down |   -  |   =  |   {  |   }  |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |Shift |      |             |------|      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,  KC_LPRN, KC_RPRN, KC_DEL,
-    KC_F1,   KC_F2,    KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_PGUP, KC_UNDS, KC_PLUS,  KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_F7,    KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_PGDN, KC_MINS, KC_EQL,   KC_LCBR, KC_RCBR, KC_PIPE,
+    KC_F1,   KC_F2,    KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_UP,   KC_UNDS, KC_PLUS,  KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_F7,    KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DOWN, KC_MINS, KC_EQL,   KC_LCBR, KC_RCBR, KC_PIPE,
     _______, _______, _______, KC_LSFT, _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 
